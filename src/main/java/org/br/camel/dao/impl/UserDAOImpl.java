305f48325975dc7366afefb817d9b4ea24caff39
Bruno Mendes <bmendes3000@gmail.com>
2017-02-13 19:58:52 -0200

    integration with jooq

--- src/main/java/org/br/camel/dao/impl/UserDAOImpl.java
+++ src/main/java/org/br/camel/dao/impl/UserDAOImpl.java
@@ -1,20 +1,58 @@
 package org.br.camel.dao.impl;
 
 import org.br.camel.dao.UserDAO;
+import org.br.camel.jooq.db.public_.tables.User;
 import org.br.camel.vos.UserVO;
+import org.jooq.DSLContext;
+import org.jooq.Record3;
+import org.jooq.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+/**
+ * Class to repository database
+ * @author Bruno Mendes
+ * @since 1.0
+ * @version 1.0
+ */
 @Repository("userDAO")
 public class UserDAOImpl implements UserDAO {
+	
+	@Autowired
+	private DSLContext dsl;
 
 	@Override
 	public void insert(final UserVO vo) {
-
+		//create new user in database
+		dsl.insertInto(User.USER)
+			.set(User.USER.ID, vo.getId())
+			.set(User.USER.NAME, vo.getName())
+			.set(User.USER.EMAIL, vo.getEmail())
+			.execute();
 	}
 
 	@Override
 	public UserVO findUserById(final Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		//variable to return
+		UserVO vo = new UserVO();
+		
+		//table for select
+		User user = User.USER.as("u");
+		
+		//Result of the select
+		Result<Record3<Integer, String, String>> result = dsl
+				 	.select(user.ID, user.NAME, user.EMAIL)
+							.from(user)
+							.where(user.ID.eq(id)).fetch();
+		
+		//foreach
+		result.forEach(record -> {
+			//put values in value object 
+			vo.setId(record.get(user.ID));
+			vo.setName(record.get(user.NAME));
+			vo.setEmail(record.get(user.EMAIL));
+		 });
+		//return result of the select in database
+		return vo;
 	}
 
 }
